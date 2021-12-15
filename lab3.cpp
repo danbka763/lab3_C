d73cc07b572dfe454cbdc0963134f39904cab25d
Danil <48606033+danbka763@users.noreply.github.com>
2021-12-15 11:47:08 +0700

    push code

---
+++ lab3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    system("chcp 1251");
+    printf("Работу выполнил Жигалин Данил Вадимович\n");
+
+    printf("Ввести x, y:\n");
+    float x, y;
+
+    int h = scanf_s("%f%f", &x, &y);
+
+    if (h == 2) {
+        if (y >= -2 && y <= 2) {
+            if ((x >= 0.5 * y && x <= -0.5 * y) ||
+                (x <= 0.5 * y && x >= -0.5 * y)
+                )
+            {
+                printf("Точка принадлежит");
+                return 0;
+            }
+        }
+
+        printf("Точка не принадлежит");
+    }
+    else {
+        printf("Введены неверные данные");
+    }
+
+    return 0;
+}
